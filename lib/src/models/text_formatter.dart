e6a053d78662f9311c5b8f27d9c29330b00dac4d
Luffynas <blankcd3@gmail.com>
2022-08-11 13:02:13 +0700

    formatting text

---
+++ lib/src/models/text_formatter.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/widgets.dart';
+
+class FormattedTextFormatter {
+  const FormattedTextFormatter({
+    required this.patternChars,
+    required this.style,
+  }) : pattern = r'\' +
+            patternChars +
+            '([^\\' +
+            patternChars +
+            ']+)\\' +
+            patternChars;
+
+  final String pattern;
+  final String patternChars;
+  final TextStyle style;
+}
+
+class FormattedTextFormatter2 extends FormattedTextFormatter {
+  const FormattedTextFormatter2({
+    required this.patternChars,
+    required this.style,
+  })  : pattern = r'\' + patternChars + '([^\\' + patternChars + ']+)',
+        super(patternChars: patternChars, style: style);
+
+  final String pattern;
+  final String patternChars;
+  final TextStyle style;
+}
